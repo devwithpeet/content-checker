6adf82766b12373828f15bfd7f14f4156d93d5ee
Peter Aba <peter@peteraba.com>
2024-11-23 14:51:16 +0100

    Course filter

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"math"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/devwithpeet/content-checker/pkg"
@@ -12,7 +13,7 @@ import (
 
 type Command string
 
-const Version = "0.1.11"
+const Version = "0.2.0"
 
 const (
 	PrintCommand   Command = "print"
@@ -21,7 +22,7 @@ const (
 	VersionCommand Command = "version"
 )
 
-func main() {
+func args() (Command, string, map[pkg.State]struct{}, bool, bool, bool, string, int) {
 	action := PrintCommand
 	if len(os.Args) > 1 {
 		action = Command(os.Args[1])
@@ -32,14 +33,66 @@ func main() {
 		root = os.Args[2]
 	}
 
+	statesAllowed := map[pkg.State]struct{}{
+		pkg.Complete:   {},
+		pkg.Incomplete: {},
+		pkg.Stub:       {},
+	}
+
+	verbose := false
+	printIndex := false
+	printNonIndex := true
+	courseWanted := ""
+	maxErrors := defaulMaxErrors
+
+	if len(os.Args) > 3 {
+		for i, arg := range os.Args[3:] {
+			switch arg {
+			case "--without-non-index", "-without-non-index":
+				printNonIndex = false
+			case "--with-index", "-with-index":
+				printIndex = true
+			case "--verbose", "-verbose":
+				verbose = true
+			case "--max-errors", "-max-errors":
+				if len(os.Args) <= i+1 {
+					panic("missing value for --max-errors")
+				}
+
+				maxErrors, _ = strconv.Atoi(os.Args[i+1])
+			case "complete":
+				statesAllowed = map[pkg.State]struct{}{
+					pkg.Complete: {},
+				}
+			case "incomplete":
+				statesAllowed = map[pkg.State]struct{}{
+					pkg.Incomplete: {},
+				}
+			case "stub":
+				statesAllowed = map[pkg.State]struct{}{
+					pkg.Stub: {},
+				}
+
+			default:
+				courseWanted = arg
+			}
+		}
+	}
+
+	return action, root, statesAllowed, verbose, printIndex, printNonIndex, courseWanted, maxErrors
+}
+
+func main() {
+	action, root, statesAllowed, verbose, printIndex, printNonIndex, courseWanted, maxErrors := args()
+
 	// collect markdown files
-	files, err := findFiles(root)
+	files, err := findFiles(root, courseWanted, verbose)
 	if err != nil {
 		panic("cannot find files in root: " + root + ", error: " + err.Error())
 	}
 
 	// fetch markdown files
-	courses, count := CrawlMarkdownFiles(files, maxErrors)
+	courses, count := CrawlMarkdownFiles(files, maxErrors, courseWanted, verbose)
 
 	Prepare(courses)
 
@@ -48,38 +101,6 @@ func main() {
 		fmt.Println("Version:", Version)
 
 	case PrintCommand:
-		statesAllowed := map[pkg.State]struct{}{
-			pkg.Complete:   {},
-			pkg.Incomplete: {},
-			pkg.Stub:       {},
-		}
-
-		printIndex := false
-		printNonIndex := true
-
-		if len(os.Args) > 3 {
-			for _, arg := range os.Args[3:] {
-				switch arg {
-				case "--without-non-index":
-					printNonIndex = false
-				case "--with-index":
-					printIndex = true
-				case "complete":
-					statesAllowed = map[pkg.State]struct{}{
-						pkg.Complete: {},
-					}
-				case "incomplete":
-					statesAllowed = map[pkg.State]struct{}{
-						pkg.Incomplete: {},
-					}
-				case "stub":
-					statesAllowed = map[pkg.State]struct{}{
-						pkg.Stub: {},
-					}
-				}
-			}
-		}
-
 		Print(count, courses, statesAllowed, printIndex, printNonIndex)
 
 	case ErrorsCommand:
@@ -93,15 +114,31 @@ func main() {
 	}
 }
 
-func findFiles(root string) ([]string, error) {
-	pattern := filepath.Join(root, "content") + "/**/**/*.md"
+func findFiles(root, courseWanted string, verbose bool) ([]string, error) {
+	if courseWanted == "" {
+		courseWanted = "**"
+	}
 
-	return filepath.Glob(pattern)
+	pattern := filepath.Join(root, "content") + "/" + courseWanted + "/**/*.md"
+
+	files, err := filepath.Glob(pattern)
+	if err != nil {
+		return nil, err
+	}
+
+	if verbose {
+		fmt.Println("Files found:")
+		for _, file := range files {
+			fmt.Println(file)
+		}
+	}
+
+	return files, nil
 }
 
-const maxErrors = 10
+const defaulMaxErrors = -1
 
-func CrawlMarkdownFiles(matches []string, maxErrors int) (pkg.Courses, int) {
+func CrawlMarkdownFiles(matches []string, maxErrors int, courseWanted string, verbose bool) (pkg.Courses, int) {
 	if maxErrors < 0 {
 		maxErrors = math.MaxInt
 	}
@@ -111,7 +148,8 @@ func CrawlMarkdownFiles(matches []string, maxErrors int) (pkg.Courses, int) {
 	var count, errCount int
 
 	for _, filePath := range matches {
-		if errCount >= maxErrors {
+		if maxErrors > 0 && errCount >= maxErrors {
+			fmt.Println("Max errors reached, stopping")
 			break
 		}
 
@@ -125,6 +163,10 @@ func CrawlMarkdownFiles(matches []string, maxErrors int) (pkg.Courses, int) {
 		course := parts[len(parts)-3]
 		chapter := parts[len(parts)-2]
 		page := parts[len(parts)-1]
+
+		// if len(courseWanted) > 0 && course != courseWanted {
+		// 	continue
+		// }
 
 		rawContent, err := os.ReadFile(filePath)
 		if err != nil {
@@ -143,6 +185,14 @@ func CrawlMarkdownFiles(matches []string, maxErrors int) (pkg.Courses, int) {
 		}
 
 		count++
+	}
+
+	if verbose {
+		fmt.Println()
+		fmt.Println("Courses:")
+		for _, course := range result {
+			fmt.Println(course.Title)
+		}
 	}
 
 	return result, count
